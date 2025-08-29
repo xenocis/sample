8934add404801c53f744a4c88fbc3d480b19a387
xendev33 <xendev33@localhost.localdomain>
2025-08-29 12:28:31 +0900

    test

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    ignores: ["node_modules", "dist"],
+  },
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+];
