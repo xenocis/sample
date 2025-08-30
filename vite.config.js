23112f5774343ae22ed043d488c8f254679a3cb2
xendev33 <xendev33@localhost.localdomain>
2025-08-30 18:18:17 +0900

    resoure path changed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/npm-experiment/",
+  //base: "/npm-experiment/",
+  base : '/sample/'
 });
